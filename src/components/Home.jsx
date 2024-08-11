c8c53a6e47c71729f4fb70f2ca42ee0c8ac178a9
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2024-08-11 22:11:27 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full max-w-[1500px] mx-auto h-full'>
+      <h1 className='uppercase text-3xl text-red-500 text-center mt-[20%]'>Welcome to our project</h1>
+    </div>
+  )
+}
+
+export default Home
